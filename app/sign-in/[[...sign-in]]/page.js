5aa85f3dc5f6c48b99754c24804e40d4f6a2911e
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 03:05:26 +0530

    a

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,15 +1,10 @@
-"use client"
 import { SignIn } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import '../../../app/globals.css';
 
 export default function Page() {
   const router = useRouter();
-  const redirectUrl = router.query?.redirectUrl || '/';
-
-  const handleOnSignIn = () => {
-    router.push(redirectUrl);
-  };
+  const redirectUrl = router.query.redirectUrl ? decodeURIComponent(router.query.redirectUrl) : '/';
 
   return (
     <div className="flex justify-center items-center h-screen">
