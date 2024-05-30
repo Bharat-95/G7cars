78d62c463b4834d46e5599907db2e5e5f27e088c
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 03:06:34 +0530

    a

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,14 +1,23 @@
-import { SignIn } from "@clerk/nextjs";
+"use client"
+import { SignIn, useAuth } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import '../../../app/globals.css';
 
 export default function Page() {
   const router = useRouter();
+  const { isSignedIn } = useAuth();
   const redirectUrl = router.query.redirectUrl ? decodeURIComponent(router.query.redirectUrl) : '/';
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push(redirectUrl);
+    }
+  }, [isSignedIn, redirectUrl, router]);
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignIn afterSignInUrl={redirectUrl} />
+      <SignIn />
     </div>
   );
 }
