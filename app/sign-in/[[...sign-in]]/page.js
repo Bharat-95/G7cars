e7d4ee3fe519d7efcae860c70c92e8574295bd0f
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 04:38:17 +0530

    a

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,13 +1,9 @@
-"use client"
 import { SignIn } from "@clerk/nextjs";
 import '../../../app/globals.css'
-import { useSearchParams } from "next/navigation";
 
-const searchParams = useSearchParams();
-const pickupDateTime = searchParams.get('pickupDateTime') ? new Date(searchParams.get('pickupDateTime')) : null;
-const dropoffDateTime = searchParams.get('dropoffDateTime') ? new Date(searchParams.get('dropoffDateTime')) : null;
+
 
 export default function Page() {
-  return <div className="flex justify-center items-center h-screen"><SignIn forceRedirectUrl="/SearchCars?pickupDateTime=${encodeURIComponent(pickupISOString)}&dropoffDateTime=${encodeURIComponent(dropoffISOString)" /></div>
+  return <div className="flex justify-center items-center h-screen"><SignIn /></div>
 }
 
