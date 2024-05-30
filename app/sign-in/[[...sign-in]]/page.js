d998a007a89aaceb483d8356470bd67201725764
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 03:24:02 +0530

    a

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -8,9 +8,12 @@ export default function Page() {
   const router = useRouter();
   const { isSignedIn } = useAuth();
   const fallbackRedirectUrl = router.query?.redirectUrl ? decodeURIComponent(router.query.redirectUrl) : '/';
-
+  
   useEffect(() => {
+    console.log("Fallback Redirect URL:", fallbackRedirectUrl);
+    console.log("isSignedIn:", isSignedIn);
     if (isSignedIn) {
+      console.log("Redirecting to:", fallbackRedirectUrl);
       router.push(fallbackRedirectUrl);
     }
   }, [isSignedIn, fallbackRedirectUrl, router]);
