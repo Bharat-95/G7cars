4ade339fccf87abc563f439b45f8a5defa8ca3a5
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 07:42:41 +0530

    a

--- app/SearchCars/page.js
+++ app/SearchCars/page.js
@@ -11,216 +11,215 @@ import { IoIosClose } from "react-icons/io";
 import { useUser } from "@clerk/clerk-react";
 import { useRouter } from "next/navigation";
 
+
 const Page = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [selectedCar, setSelectedCar] = useState(null);
-  const [price, setPrice] = useState(0);
-  const [discount, setDiscount] = useState(0);
-  const [showConfirmation, setShowConfirmation] = useState(false);
-  const [bookedCars, setBookedCars] = useState({});
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [selectedCar, setSelectedCar] = useState(null);
+    const [price, setPrice] = useState(0);
+    const [discount, setDiscount] = useState(0);
+    const [showConfirmation, setShowConfirmation] = useState(false);
 
-  const { isSignedIn } = useUser();
-  const router = useRouter();
+    const { isSignedIn } = useUser();
+    const router = useRouter();
 
-  const searchParams = useSearchParams();
-  const pickupDateTimeParam = searchParams.get('pickupDateTime');
-  const dropoffDateTimeParam = searchParams.get('dropoffDateTime');
-  const pickupDateTime = pickupDateTimeParam ? new Date(pickupDateTimeParam) : null;
-  const dropoffDateTime = dropoffDateTimeParam ? new Date(dropoffDateTimeParam) : null;
+    const searchParams = useSearchParams();
+    const pickupDateTime = searchParams.get('pickupDateTime') ? new Date(searchParams.get('pickupDateTime')) : null;
+    const dropoffDateTime = searchParams.get('dropoffDateTime') ? new Date(searchParams.get('dropoffDateTime')) : null;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://pvmpxgfe77.execute-api.us-east-1.amazonaws.com/cars");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const data = await response.json();
-        setData(data);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    "https://pvmpxgfe77.execute-api.us-east-1.amazonaws.com/cars"
+                );
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+                const data = await response.json();
+                setData(data);
+                setLoading(false);
+            } catch (error) {
+                setError(error.message);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const saveBookingDataToLocalstorage = () => {
+        localStorage.setItem('pickupDateTime', pickupDateTime);
+        localStorage.setItem('dropDateTime', dropoffDateTime);
     };
 
-    fetchData();
-  }, []);
+    const handleBookCar = (car) => {
+        if (!pickupDateTime || !dropoffDateTime) {
+            alert("Please select pickup/drop-off dates");
+            return;
+        }
 
-  const saveBookingDataToLocalstorage = () => {
-    localStorage.setItem('pickupDateTime', pickupDateTime);
-    localStorage.setItem('dropDateTime', dropoffDateTime);
-  };
+        const days = Math.ceil((dropoffDateTime - pickupDateTime) / (1000 * 60 * 60 * 24));
+        const carPrice = parseFloat(car.Price.replace(/[^\d.-]/g, ''));
+        let totalPrice = carPrice * days;
+        let discountAmount = 0;
 
-  const handleBookCar = (car) => {
-    if (!pickupDateTime || !dropoffDateTime) {
-      alert("Please select pickup/drop-off dates");
-      return;
-    }
+        if (days >= 10) {
+            discountAmount = totalPrice * 0.1; 
+            totalPrice *= 0.9;
+        } else if (days >= 4) {
+            discountAmount = totalPrice * 0.05; 
+            totalPrice *= 0.95;
+        }
 
-    const days = Math.ceil((dropoffDateTime - pickupDateTime) / (1000 * 60 * 60 * 24));
-    const carPrice = parseFloat(car.Price.replace(/[^\d.-]/g, ''));
-    let totalPrice = carPrice * days;
-    let discountAmount = 0;
+        setSelectedCar(car);
+        setPrice(totalPrice);
+        setDiscount(discountAmount);
+        setShowConfirmation(true);
 
-    if (days >= 10) {
-      discountAmount = totalPrice * 0.1;
-      totalPrice *= 0.9;
-    } else if (days >= 4) {
-      discountAmount = totalPrice * 0.05;
-      totalPrice *= 0.95;
-    }
+        
+        saveBookingDataToLocalstorage();
+    };
 
-    setSelectedCar(car);
-    setPrice(totalPrice);
-    setDiscount(discountAmount);
-    setShowConfirmation(true);
+    const confirmBooking = () => {
+        const currentUrl = window.location.href;
+        if (isSignedIn) {
+            router.push('payment');
+        } else {
+            router.push(`/sign-in`);
+        }
+    };
 
-    saveBookingDataToLocalstorage();
-  };
+    const cancelConfirmation = () => {
+        setShowConfirmation(false);
+        setSelectedCar(null);
+        setPrice(0);
+        setDiscount(0);
+    };
+    return (
+        <div className="min-h-screen bg-white">
+            <Header />
+            <div className="flex justify-center m-10 text-4xl font-bold underline text-rose-900">
+                Cars List
+            </div>
+            <div className="lg:grid lg:grid-cols-4 md:grid md:grid-cols-2">
+                {data.map((car) => (
+                    <div key={car.G7cars123} className="m-20 cursor-pointer">
+                        <div className="h-96 w-64 duration-500 p-4 rounded-xl hover:border-[2px] hover:border-rose-950 space-y-4">
+                            <div className="font-semibold text-xl text-rose-900">
+                                {car.Name}
+                            </div>
+                            <div>{car.Price}/day</div>
+                            <div>
+                                <Image
+                                    src={car.Coverimage[0]}
+                                    width={0}
+                                    height={0}
+                                    alt="No Image Found"
+                                    className="w-48 h-36"
+                                    unoptimized
+                                />
+                            </div>
+                            <div className="gap-4 flex justify-evenly">
+                                <div className="flex flex-col items-center">
+                                    <TbManualGearboxFilled
+                                        size={24}
+                                        className="text-gray-400"
+                                    />
+                                    <div className="text-[10px]">{car.Gear}</div>
+                                </div>
+                                <div className="flex flex-col items-center">
+                                    <MdOutlineAirlineSeatReclineNormal
+                                        size={24}
+                                        className="text-gray-400 space-y-4"
+                                    />
+                                    <div className="text-[10px]">{car.Seating}</div>
+                                </div>
+                                <div className="flex flex-col items-center">
+                                    <BsFillFuelPumpFill
+                                        size={24}
+                                        className="text-gray-400 space-y-4"
+                                    />
+                                    <div className="text-[10px]">{car.Fuel}</div>
+                                </div>
+                            </div>
+                            <button
+                                onClick={() => handleBookCar(car)}
+                                className="flex justify-center py-4 px-2 bg-rose-900 text-white rounded-md hover:bg-rose-900/95 w-52"
+                            >
+                                Rent car
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
 
-  const confirmBooking = () => {
-    const currentUrl = window.location.href;
-    if (isSignedIn) {
-      setBookedCars((prevBookedCars) => ({ ...prevBookedCars, [selectedCar.G7cars123]: true }));
-      router.push('payment');
-    } else {
-      router.push(`/sign-in?redirectUrl=${encodeURIComponent(currentUrl)}`);
-    }
-  };
+            {showConfirmation && (
+                <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+                    <div className="bg-white p-8 rounded-xl">
+                        <div className="flex justify-end">
+                            <button onClick={cancelConfirmation}>
+                               <IoIosClose size={30}/>
+                            </button>
+                        </div>
+                        <h2 className="text-2xl font-semibold text-rose-900">
+                            Confirmation
+                        </h2>
+                        <div>
+                            <p>
+                                <strong>Car:</strong> {selectedCar.Name}
+                            </p>
+                            <p>
+                                <strong>Pick-up Date & Time:</strong>{" "}
+                                {pickupDateTime.toLocaleString(undefined, {
+                                    year: "numeric",
+                                    month: "long",
+                                    day: "numeric",
+                                    hour: "numeric",
+                                    minute: "numeric",
+                                    hour12: true,
+                                })}
+                            </p>
+                            <p>
+                                <strong>Drop-off Date & Time:</strong>{" "}
+                                {dropoffDateTime.toLocaleString(undefined, {
+                                    year: "numeric",
+                                    month: "long",
+                                    day: "numeric",
+                                    hour: "numeric",
+                                    minute: "numeric",
+                                    hour12: true,
+                                })}
+                            </p>
+                            <p>
+                                <strong>Total Days:</strong> {Math.ceil((dropoffDateTime - pickupDateTime) / (1000 * 60 * 60 * 24))}
+                            </p>
+                            <p>
+                                <strong>Total Price:</strong> ₹ {price.toFixed(2)}
+                            </p>
+                            {discount > 0 && (
+                                <p>
+                                    <strong>Discount:</strong> ₹ {discount.toFixed(2)}
+                                </p>
+                            )}
+                        </div>
+                        <div className="mt-4 flex justify-center">
+                            <button
+                                onClick={confirmBooking}
 
-  const cancelConfirmation = () => {
-    setShowConfirmation(false);
-    setSelectedCar(null);
-    setPrice(0);
-    setDiscount(0);
-  };
-
-  return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <div className="flex justify-center m-10 text-4xl font-bold underline text-rose-900">
-        Cars List
-      </div>
-      <div className="lg:grid lg:grid-cols-4 md:grid md:grid-cols-2">
-        {data.map((car) => (
-          <div key={car.G7cars123} className="m-20 cursor-pointer">
-            <div className="h-96 w-64 duration-500 p-4 rounded-xl hover:border-[2px] hover:border-rose-950 space-y-4">
-              <div className="font-semibold text-xl text-rose-900">
-                {car.Name}
-              </div>
-              <div>{car.Price}/day</div>
-              <div>
-                <Image
-                  src={car.Coverimage[0]}
-                  width={0}
-                  height={0}
-                  alt="No Image Found"
-                  className="w-48 h-36"
-                  unoptimized
-                />
-              </div>
-              <div className="gap-4 flex justify-evenly">
-                <div className="flex flex-col items-center">
-                  <TbManualGearboxFilled
-                    size={24}
-                    className="text-gray-400"
-                  />
-                  <div className="text-[10px]">{car.Gear}</div>
+                                className="px-4 py-2 bg-rose-900 text-white rounded-md hover:bg-rose-900/95"
+                            >
+                                Confirm Booking
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex flex-col items-center">
-                  <MdOutlineAirlineSeatReclineNormal
-                    size={24}
-                    className="text-gray-400 space-y-4"
-                  />
-                  <div className="text-[10px]">{car.Seating}</div>
-                </div>
-                <div className="flex flex-col items-center">
-                  <BsFillFuelPumpFill
-                    size={24}
-                    className="text-gray-400 space-y-4"
-                  />
-                  <div className="text-[10px]">{car.Fuel}</div>
-                </div>
-              </div>
-              <button
-                onClick={() => handleBookCar(car)}
-                className={`flex justify-center py-4 px-2 bg-rose-900 text-white rounded-md hover:bg-rose-900/95 w-52 ${bookedCars[car.G7cars123] ? "opacity-50 cursor-not-allowed" : ""}`}
-                disabled={bookedCars[car.G7cars123]}
-              >
-                {bookedCars[car.G7cars123] ? "Car Booked" : "Rent Car"}
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+            )}
 
-      {showConfirmation && (
-        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-          <div className="bg-white p-8 rounded-xl">
-            <div className="flex justify-end">
-              <button onClick={cancelConfirmation}>
-                <IoIosClose size={30} />
-              </button>
-            </div>
-            <h2 className="text-2xl font-semibold text-rose-900">
-              Confirmation
-            </h2>
-            <div>
-              <p>
-                <strong>Car:</strong> {selectedCar.Name}
-              </p>
-              <p>
-                <strong>Pick-up Date & Time:</strong>{" "}
-                {pickupDateTime.toLocaleString(undefined, {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                  hour: "numeric",
-                  minute: "numeric",
-                  hour12: true,
-                })}
-              </p>
-              <p>
-                <strong>Drop-off Date & Time:</strong>{" "}
-                {dropoffDateTime.toLocaleString(undefined, {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                  hour: "numeric",
-                  minute: "numeric",
-                  hour12: true,
-                })}
-              </p>
-              <p>
-                <strong>Total Days:</strong> {Math.ceil((dropoffDateTime - pickupDateTime) / (1000 * 60 * 60 * 24))}
-              </p>
-              <p>
-                <strong>Total Price:</strong> ₹ {price.toFixed(2)}
-              </p>
-              {discount > 0 && (
-                <p>
-                  <strong>Discount:</strong> ₹ {discount.toFixed(2)}
-                </p>
-              )}
-            </div>
-            <div className="mt-4 flex justify-center">
-              <button
-                onClick={confirmBooking}
-                className="px-4 py-2 bg-rose-900 text-white rounded-md hover:bg-rose-900/95"
-              >
-                Confirm Booking
-              </button>
-            </div>
-          </div>
+            <Footer />
         </div>
-      )}
-
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default Page;
--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,37 +1,9 @@
-"use client"
-import { SignIn, useUser } from "@clerk/nextjs";
-import '../../../app/globals.css';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { SignIn } from "@clerk/nextjs";
+import '../../../app/globals.css'
+
+
 
 export default function Page() {
-  const { isSignedIn } = useUser();
-  const router = useRouter();
-  const { pickupDateTime, dropoffDateTime } = router.query;
-
-  const redirectUrl = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL || '/search-cars';
-  const fallbackRedirectUrl = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL || '/search-cars';
-
-  useEffect(() => {
-    if (isSignedIn) {
-      const newUrl = `${redirectUrl}?pickupDateTime=${pickupDateTime}&dropoffDateTime=${dropoffDateTime}`;
-      router.push(newUrl);
-    }
-  }, [isSignedIn, redirectUrl, pickupDateTime, dropoffDateTime, router]);
-
-  if (isSignedIn) {
-    // Return null to prevent rendering the SignIn component if the user is already signed in
-    return null;
-  }
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <SignIn
-        path="/sign-in"
-        routing="path"
-        signUpUrl="/sign-up"
-        redirectUrl={`${redirectUrl}?pickupDateTime=${pickupDateTime}&dropoffDateTime=${dropoffDateTime}`}
-      />
-    </div>
-  );
+  return <div className="flex justify-center items-center h-screen"><SignIn /></div>
 }
+
