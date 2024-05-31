d9f985f7d76d570f178dce4ed04b09c37c1dad0e
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 07:49:00 +0530

    A

--- app/payment/page.js
+++ app/payment/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-3xl font-bold flex justify-center items-center'>
+    <div className='text-3xl font-bold flex justify-center items-center text-white h-screen p-10'>
        We are working to integrate payment gateway. Thank you for understanding
     </div>
   )
