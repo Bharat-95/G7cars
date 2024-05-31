ed7b29c3b98352a5e80f3e91c0bd5dd4abefd56d
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 06:33:18 +0530

    a

--- app/payment/page.js
+++ app/payment/page.js
@@ -1,35 +1,11 @@
-import { genchecksum } from "../../utils/checksum";
-import PaytmConfig from "../../paytmConfig";
+import React from 'react'
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { amount, customerId, email, phone } = req.body;
-
-    let params = {
-      MID: PaytmConfig.MID,
-      WEBSITE: PaytmConfig.WEBSITE,
-      INDUSTRY_TYPE_ID: PaytmConfig.INDUSTRY_TYPE_ID,
-      CHANNEL_ID: PaytmConfig.CHANNEL_ID,
-      ORDER_ID: "ORDER_" + new Date().getTime(),
-      CUST_ID: customerId,
-      TXN_AMOUNT: amount,
-      CALLBACK_URL: PaytmConfig.CALLBACK_URL,
-      EMAIL: email,
-      MOBILE_NO: phone,
-    };
-
-    genchecksum(params, PaytmConfig.KEY, (err, checksum) => {
-      if (err) {
-        return res.status(500).json({ error: "Checksum generation failed" });
-      }
-      params["CHECKSUMHASH"] = checksum;
-
-      res.status(200).json({
-        params: params,
-        txnUrl: PaytmConfig.TXN_URL,
-      });
-    });
-  } else {
-    res.status(405).json({ error: "Method not allowed" });
-  }
+const page = () => {
+  return (
+    <div>
+        Payment page
+    </div>
+  )
 }
+
+export default page
