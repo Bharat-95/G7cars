bcfff34adf1257ab60894e447993a01fb73a1035
G7cars <g7selfdrivecars@gmail.com>
2024-05-31 07:06:48 +0530

    a

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,9 +1,30 @@
+"use client"
 import { SignIn } from "@clerk/nextjs";
-import '../../../app/globals.css'
-
-
+import '../../../app/globals.css';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Page() {
-  return <div className="flex justify-center items-center h-screen"><SignIn /></div>
-}
+  const router = useRouter();
+  const { redirectUrl, pickupDateTime, dropoffDateTime } = router.query;
 
+  useEffect(() => {
+    // If the user is already signed in, redirect them to the appropriate URL
+    if (router.isReady && redirectUrl) {
+      const newUrl = `${redirectUrl}?pickupDateTime=${pickupDateTime}&dropoffDateTime=${dropoffDateTime}`;
+      router.push(newUrl);
+    }
+  }, [router.isReady, redirectUrl, pickupDateTime, dropoffDateTime]);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignIn
+        afterSignInUrl={
+          redirectUrl 
+            ? `${redirectUrl}?pickupDateTime=${pickupDateTime}&dropoffDateTime=${dropoffDateTime}`
+            : '/'
+        }
+      />
+    </div>
+  );
+}
