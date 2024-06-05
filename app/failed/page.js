069a80e5bdb93870dad38ff5b5337e3dbaa4d8df
G7cars <g7selfdrivecars@gmail.com>
2024-06-05 17:27:13 +0530

    a

--- app/failed/page.js
+++ app/failed/page.js
@@ -1,12 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/');
+    }, 60000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div>
-
-        Payment Failed please try again.....
+      Payment Failed, please try again.....
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
