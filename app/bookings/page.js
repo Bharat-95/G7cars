893cbc3d52fd0ebce6843b16cb17be467bc05fc2
G7cars <g7selfdrivecars@gmail.com>
2024-07-01 23:19:28 +0530

    a

--- app/bookings/page.js
+++ app/bookings/page.js
@@ -6,18 +6,34 @@ import Footer from '../Footer';
 
 const Page = () => {
   const [data, setData] = useState([]);
+  const [carDetails, setCarDetails] = useState({});
   const { user } = useUser();
 
   console.log(user.id)
 
   const fetchData = async () => {
     try {
-      const url = `https://pvmpxgfe77.execute-api.us-east-1.amazonaws.com/bookings/${user.id}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data);
+      const bookingsUrl = `https://pvmpxgfe77.execute-api.us-east-1.amazonaws.com/bookings/${user.id}`;
+      const bookingsResponse = await fetch(bookingsUrl);
+      const bookingsData = await bookingsResponse.json();
+      setData(bookingsData);
+
+      const carDetailsPromises = bookingsData.map(async (booking) => {
+        const carUrl = `https://pvmpxgfe77.execute-api.us-east-1.amazonaws.com/cars/${booking.carId}`;
+        const carResponse = await fetch(carUrl);
+        const carData = await carResponse.json();
+        return { carId: booking.carId, carData };
+      });
+
+      const carDetailsArray = await Promise.all(carDetailsPromises);
+      const carDetailsObject = carDetailsArray.reduce((acc, { carId, carData }) => {
+        acc[carId] = carData;
+        return acc;
+      }, {});
+
+      setCarDetails(carDetailsObject);
     } catch (error) {
-      console.error('Error fetching bookings:', error);
+      console.error('Error fetching data:', error);
     }
   };
 
@@ -33,21 +49,27 @@ const Page = () => {
 
   return (
     <div>
-        <Header />
+      <Header />
       {data.length > 0 ? (
         data.map((booking) => (
-          <div key={booking.bookingId} className='text-white justify-center items-center'> 
+          <div key={booking.bookingId} className='text-white justify-center items-center'>
             <div>Booking ID: {booking.bookingId}</div>
             <div>Car ID: {booking.carId}</div>
             <div>Pickup DateTime: {booking.pickupDateTime}</div>
             <div>Dropoff DateTime: {booking.dropoffDateTime}</div>
             <div>Payment ID: {booking.paymentId}</div>
+            {carDetails[booking.carId] && (
+              <div>
+                <div>Car Name: {carDetails[booking.carId].name}</div>
+                <img src={carDetails[booking.carId].imageUrl} alt={carDetails[booking.carId].name} />
+              </div>
+            )}
           </div>
         ))
       ) : (
         <div>No bookings found</div>
       )}
-      <Footer/>
+      <Footer />
     </div>
   );
 };
