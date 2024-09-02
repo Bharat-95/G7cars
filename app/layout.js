f17139b80abac025e78bf61dd60717d7ddcd8a43
G7cars <g7selfdrivecars@gmail.com>
2024-09-02 11:38:27 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-
-
-
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,12 +11,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>   
+    <ClerkProvider>
       <link rel="icon" href="../public/favicon.ico" />
-       <html lang="en" className="bg-rose-950 w-screen">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en" className="bg-rose-950 w-screen">
+        <body className={inter.className}>
+          <SignedIn>
+            {children}
+          </SignedIn>
+          <SignedOut>
+            <p>You must be signed in to view this content.</p>
+          </SignedOut>
+        </body>
+      </html>
     </ClerkProvider>
-
   );
 }
