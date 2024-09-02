53cd2d277ec55d4556478d160a3c1b1099b6f015
G7cars <g7selfdrivecars@gmail.com>
2024-09-02 11:41:53 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
