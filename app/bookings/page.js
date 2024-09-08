b7bbf30a12a02274592f1d72e55e84e16b059eac
G7cars <g7selfdrivecars@gmail.com>
2024-09-08 13:55:34 +0530

    a

--- app/bookings/page.js
+++ app/bookings/page.js
@@ -43,8 +43,15 @@ const Page = () => {
     }
   }, [isLoaded, user]);
 
-  const formatDate = (dateString) => {
-    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+  const formatDateTime = (dateString) => {
+    const options = {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true, 
+    };
     return new Intl.DateTimeFormat('en-GB', options).format(new Date(dateString));
   };
 
