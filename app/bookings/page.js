e939cecd4094fe3d54a87cf149c542048fc9bb68
G7cars <g7selfdrivecars@gmail.com>
2024-09-08 13:57:55 +0530

    a

--- app/bookings/page.js
+++ app/bookings/page.js
@@ -43,7 +43,7 @@ const Page = () => {
     }
   }, [isLoaded, user]);
 
-  const formatDateTime = (dateString) => {
+  const formatDate = (dateString) => {
     const options = {
       day: '2-digit',
       month: '2-digit',
