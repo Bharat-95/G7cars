3b14fb95ffff8d9c2668f24457fd90bb13062054
G7cars <g7selfdrivecars@gmail.com>
2024-09-08 13:53:11 +0530

    a

--- app/bookings/page.js
+++ app/bookings/page.js
@@ -92,7 +92,7 @@ const Page = () => {
                   {booking.status === 'Active' && (
                     <button
                       onClick={() => handleExtendBooking(booking.bookingId)}
-                      className="mt-2 bg-blue-500 hover:bg-black text-white font-bold py-2 px-4 rounded"
+                      className="mt-2 bg-black text-white font-bold py-2 px-4 rounded"
                     >
                       Extend Booking
                     </button>
