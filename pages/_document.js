cc8a9d88810422b41f3ec3b08b84ed52488975f0
G7cars <g7selfdrivecars@gmail.com>
2024-09-13 22:33:42 +0530

    a

--- pages/_document.js
+++ pages/_document.js
@@ -2,17 +2,23 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0GEHKTG50P"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0GEHKTG50P');
-          `,
-        }} />
+        {/* Google Analytics Tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-0GEHKTG50P"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0GEHKTG50P');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
